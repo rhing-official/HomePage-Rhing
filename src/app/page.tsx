cb9d3c893c2468178942a3ea711ef96a39db559a
rhing-official <rhing.official@gmail.com>
2026-07-03 01:06:55 +0900

    最終修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ const floatingItems: FloatingItem[] = [
     {
         // mobileの記述がないため、スマホ（mobile）時は自動的に表示されません
         tabletPortrait: { top: 60, left: 0 },
-        tabletLandscape: { top: 55, left: 10 },
+        tabletLandscape: { top: 50, left: 10 },
         desktop: { top: 48, left: 16 },
         width: "w-40", delay: 0.4, duration: 7.5, rot: 4, src: "/view/5.jpg"
     },
